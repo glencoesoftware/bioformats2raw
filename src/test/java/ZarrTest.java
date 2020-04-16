4120377431636178fd0c5b46544cc8d7f94d1688
jmoore <josh@glencoesoftware.com>
2020-04-16 09:47:05 +0200

    Add test copyright

--- src/test/java/ZarrTest.java
+++ src/test/java/ZarrTest.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2019 Glencoe Software, Inc. All rights reserved.
+ *
+ * This software is distributed under the terms described by the LICENSE.txt
+ * file you can find at the root of the distribution bundle.  If the file is
+ * missing please request a copy by contacting info@glencoesoftware.com
+ */
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
