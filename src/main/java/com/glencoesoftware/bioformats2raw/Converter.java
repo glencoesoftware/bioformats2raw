5559190f5dc4ab076eb01b72953c1f1629e3ec8b
Melissa Linkert <melissa@glencoesoftware.com>
2020-08-31 11:03:03 -0500

    Add --overwrite option
    By default, it is now fatal if the output directory exists.
    Using --overwrite will attempt to delete the output file/directory
    before writing.  If the delete is not successful, an IOException will
    be thrown.

--- src/main/java/com/glencoesoftware/bioformats2raw/Converter.java
+++ src/main/java/com/glencoesoftware/bioformats2raw/Converter.java
@@ -326,6 +326,12 @@ public class Converter implements Callable<Void> {
   )
   private volatile File memoDirectory;
 
+  @Option(
+          names = "--overwrite",
+          description = "Overwrite the output directory if it exists"
+  )
+  private volatile boolean overwrite = false;
+
   /** Scaling implementation that will be used during downsampling. */
   private volatile IImageScaler scaler = new SimpleImageScaler();
 
@@ -379,7 +385,12 @@ public class Converter implements Callable<Void> {
     }
 
     if (Files.exists(outputPath)) {
-      LOGGER.warn("Output path {} already exists", outputPath);
+      if (!overwrite) {
+        throw new IllegalArgumentException(
+          "Output path " + outputPath + " already exists");
+      }
+      LOGGER.warn("Overwriting output path {}", outputPath);
+      deleteDirectory(outputPath);
     }
 
     readers = new ArrayBlockingQueue<IFormatReader>(maxWorkers);
@@ -1101,6 +1112,23 @@ public class Converter implements Callable<Void> {
     catch (ServiceException se) {
       throw new FormatException(se);
     }
+  }
+
+  private void deleteDirectory(Path dir) throws IOException {
+    if (!Files.exists(dir)) {
+      return;
+    }
+    if (Files.isDirectory(dir)) {
+      Files.list(dir).forEach(p -> {
+        try {
+          deleteDirectory(p);
+        }
+        catch (IOException e) {
+          LOGGER.debug("Could not delete " + p, e);
+        }
+      });
+    }
+    Files.delete(dir);
   }
 
   private Slf4JStopWatch stopWatch() {
