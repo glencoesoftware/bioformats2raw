878a586adff3d08c0eebff30b43d3f82d808e4de
Sebastien Besson <seb.besson@gmail.com>
2022-04-27 15:35:55 +0100

    Use ImageRef to assign each series to a WellSample
    The previous codepath was relying on the assumption that the index of
    each WellSample could be mapped to each series via its index. While this
    applies for the majority of the HCS readers, it is possible to craft an
    OME-XML more liberally according to the specification.
    This commit proposes to update the test by using WellSamples.ImageRef
    and Image.ID as the absolute source of truth.

--- src/main/java/com/glencoesoftware/bioformats2raw/HCSIndex.java
+++ src/main/java/com/glencoesoftware/bioformats2raw/HCSIndex.java
@@ -29,7 +29,7 @@ public class HCSIndex {
     for (int p=0; p<meta.getPlateCount(); p++) {
       for (int w=0; w<meta.getWellCount(p); w++) {
         for (int ws=0; ws<meta.getWellSampleCount(p, w); ws++) {
-          if (meta.getWellSampleIndex(p, w, ws).getValue() == series) {
+          if (meta.getWellSampleImageRef(p, w, ws) == meta.getImageID(series)) {
             plate = p;
             field = ws;
             wellRow = meta.getWellRow(p, w).getValue();
